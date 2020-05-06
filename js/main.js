37706e553e7262c040962028d2fcf7813fdb7483
Ripon75 <57796485+Ripon75@users.noreply.github.com>
2020-05-06 13:19:11 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@ var game;
 window.onload = function(){
     var config = {
         type: Phaser.AUTO,
-        width: 500,
-        height: 600,
+        scale: {
+          mode: Phaser.Scale.FIT, //SMOTH
+          parent: 'phaser-example',
+          autoCenter: Phaser.Scale.CENTER_BOTH,
+          width: 360,
+          height: 640
+        },
         physics: {
             default: 'arcade',
             arcade: {
-                //gravity: { y: 100 },
+               
                 debug: false
             }
         },
         parent: 'div-tag-name',
+        backgroundColor: 0xa0a0a0,
         scene: [mainScene,sceneOver]
     };   
     game = new Phaser.Game(config);
