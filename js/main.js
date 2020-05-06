dcc039bc414b306f8399adf52073fcf545931afd
Ripon75 <57796485+Ripon75@users.noreply.github.com>
2020-05-06 13:25:35 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 var game;
 window.onload = function(){
+
+    FBInstant.initializeAsync().then(function () {
+
+        var progress = 0;
+        var inerval = setInterval(function () {
+          progress += 10;
+          FBInstant.setLoadingProgress(progress);
+          if (progress >= 90) {
+            clearInterval(inerval);
+            FBInstant.startGameAsync().then(function () {
+    
+              var contextId = FBInstant.context.getID();
+    
+              var contextType = FBInstant.context.getType();
+    
+              var playerName = FBInstant.player.getName();
+              var playerPic = FBInstant.player.getPhoto();
+              var playerId = FBInstant.player.getID();
+            });
+    
+          }
+    
+        }, 100);
+    
+      });
+
     var config = {
         type: Phaser.AUTO,
         scale: {
